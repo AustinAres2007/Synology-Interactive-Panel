9c61d0e76846a3cab6b0a94240e2174059b33792
Ausitin Ares <62123783+AustinAres2007@users.noreply.github.com>
2022-05-24 13:56:38 +0100

    Added comments and some wording changes

--- occ.py
+++ occ.py
@@ -16,9 +16,9 @@ class OfflineCacheConstructor:
         self.files = None
         self._c_handler = cache_handler
         
-    def get_content(self, path: str='', dataset: dict=None, return_error=True, boolean=False, seperator: str='/', _old_name='root', _identifier=None) -> Union[bool, dict, None]:
+    def get_content(self, path: str='', dataset: dict=None, return_error=True, boolean=False, _old_name='root') -> Union[bool, dict, None]:
         
-        dirs = path.split(seperator)
+        dirs = path.split('/')
         x = None
         
         if type(dataset) in [dict, list] and dirs[0]:
@@ -29,7 +29,7 @@ class OfflineCacheConstructor:
                 
             if dirs[0] in (dataset if type(dataset) == dict else (dataset[x] if x else [])):
 
-                return self.get_content(seperator.join(dirs[1:]), dataset.get(dirs[0]) if type(dataset) == dict else dataset[x].get(dirs[0]), return_error, boolean, seperator, dirs[0], _identifier=_identifier) if not boolean else True
+                return self.get_content('/'.join(dirs[1:]), dataset.get(dirs[0]) if type(dataset) == dict else dataset[x].get(dirs[0]), return_error, boolean, dirs[0]) if not boolean else True
                 
             else:
                 
@@ -45,7 +45,7 @@ class OfflineCacheConstructor:
         return {tree_list[0]: [[], self._build_tree(tree_list[1:])]} if tree_list else []
         
     def build_dir(self, folders: list or str, dataset: dict):
-        folders = folders if isinstance(folders, list) else _Path(folders).parts
+        folders = folders if isinstance(folders, list) else _Path(folders).parts # Gets the folders to be made
         
         if folders:
             
@@ -53,8 +53,8 @@ class OfflineCacheConstructor:
             n = 1
             where = ""
             
-            for i, fd in enumerate(folders, start=n):
-                dir_contents = self.get_content('/'.join(folders[:i-1]), dataset, return_error=False, _identifier='0')
+            for i, fd in enumerate(folders, start=n): # For each new folder in folders
+                dir_contents = self.get_content('/'.join(folders[:i-1]), dataset, return_error=False)
                 dir_contents = dir_contents if type(dir_contents) == dict else dir_contents[1]
                 
                 if fd not in dir_contents:
@@ -64,61 +64,71 @@ class OfflineCacheConstructor:
                     
                     break
             
-            if flag:
-                data = self.get_content(where, dataset, _identifier='1')
+            if flag: # if any folders have been made
+                data = self.get_content(where, dataset)
                 try:
-                    data[folders[n-1]] = [[], self._build_tree(folders[n:])]
+                    data[folders[n-1]] = [[], self._build_tree(folders[n:])] # Adds a new folder to the path
                 except TypeError:
-                    data.append({folders[n-1]: [[], self._build_tree(folders[n:])] })
-                finally: 
-                    return dataset
+                    data.append({folders[n-1]: [[], self._build_tree(folders[n:])] }) # Does the same as the try block, but this is only used for the root
+                finally: return dataset # Return new structure
+                
             return dataset
             
         raise ValueError('folders list argument is empty.')
         
     def dump_at_location(self, file: str, location: str, table: dict):
         
-        location += '/'
-        dir = self.get_content(path=location, dataset=table, _identifier='3')
-        dir.append(file)
+        location += '/' # Appends a slash at the end of the path
+        dir = self.get_content(path=location, dataset=table) # Gets the contents of the given folder
+        dir.append(file) # Appends the filename to the folder contents (dir)
         
         return table
     
     def build_offline_structure(self):
         
+        """
+        
+        Builds a dict upon the structure in occ.json.
+        You cannot use this file directly, it is only used within sip.py
+        
+        No arguments can be given to this function.
+        
+        """
+        
         folders = []
         main_data = []
         self._final_dict = {}
         
-        def new_cache():
+        def new_cache(): # Writes new occ.json file
             with open(self.cache_file, 'w+') as cr_cache_file:
                 _json.dump({}, cr_cache_file, indent=5)
                 
         try:
-            with open(self.cache_file, 'r') as demo_file:
+            with open(self.cache_file, 'r') as demo_file: # Only reading
                 
                     data: dict = _json.loads(demo_file.read())
-                    self.data = data
+                    self.data = data # Main occ.json, unparsed
                     
                     for d, f in data.values():
-                        folders.append(d) if d not in folders else None
-                        main_data.append((d, f))
-        except _json.decoder.JSONDecodeError as e:
-            print(f"Cache file corrupted. (occ.json within where this program is stored)")
-            new_cache()
-            self._c_handler.clear_cache()
+                        folders.append(d) if d not in folders else None # For all folders in occ.json
+                        main_data.append((d, f)) # For all the files in occ.json
+                        
+        except _json.decoder.JSONDecodeError as e: # This can occur if the user has edited occ.json
+            print(f"Cache file corrupted. Erasing and restoring..")
+            new_cache() # New cache
+            self._c_handler.clear_cache() # Remove all files in the cache
             
-        except FileNotFoundError:
-            new_cache()
+        except FileNotFoundError: # If the cache does not exist, make a new one
+            new_cache() 
             
-        for folder in folders:
-            self._final_dict = self.build_dir(folder, self._final_dict)
+        for folder in folders: # for each folder
+            self._final_dict = self.build_dir(folder, self._final_dict) # Build a file structure upon each folder
         
-        for folder, file in main_data:
+        for folder, file in main_data: # Gets each folder & file and deposits it at the given location
             self._final_dict = self.dump_at_location(file, folder, self._final_dict)
 
     
-        self.files = self._final_dict
+        self.files = self._final_dict # This is the return value.
         return True
         
     def change_cache_index(self, mode: str=None, ids: list=[], *args):
@@ -141,15 +151,3 @@ class OfflineCacheConstructor:
         new_cache_file = open(self.cache_file, 'w')    
         _json.dump(cache_contents, new_cache_file, indent=5)
         new_cache_file.close()
-
-if __name__ == '__main__':
-    print("Commensing run test.")
-    
-    test_obj = OfflineCacheConstructor("occ.json")
-    test_obj.build_offline_structure()
-    
-    _path = input('Please input a path to commence final test upon: ')
-
-    print(f"FINAL TEST FROM PATH: {_path}\n\n: {test_obj.get_content(_path, test_obj.files)}")
-    
-    
